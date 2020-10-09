d821ebc3fb864c0c1a1b481f649437d72b46099f
Filipe Regadas <filiperegadas@gmail.com>
2020-10-10 00:14:37 +0100

    Add k8s mode

--- packages.el
+++ packages.el
@@ -9,6 +9,7 @@
 ;; To install SOME-PACKAGE from MELPA, ELPA or emacsmirror:
 ;(package! some-package)
 (package! dhall-mode)
+(package! k8s-mode)
 
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
