511030efc9cfd3d285c5f37b02cf92b3785e0f18
Filipe Regadas <filiperegadas@gmail.com>
2020-11-13 17:42:16 +0000

    Add ocaml

--- init.el
+++ init.el
@@ -146,7 +146,7 @@
        markdown          ; writing docs for people to ignore
        ;;nim               ; python + lisp at the speed of c
        nix               ; I hereby declare "nix geht mehr!"
-       ;;ocaml             ; an objective camel
+       (ocaml +lsp)             ; an objective camel
        (org
         +pretty
         +dragndrop
