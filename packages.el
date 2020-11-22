d0c5a2d52cdb35ea0824dad8bef4526f9920748f
Filipe Regadas <filiperegadas@gmail.com>
2020-11-22 15:49:46 +0000

    Add protobuf-mode

--- packages.el
+++ packages.el
@@ -10,6 +10,7 @@
 ;(package! some-package)
 (package! dhall-mode)
 (package! k8s-mode)
+(package! protobuf-mode)
 
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
