fc646fe2e86fcda70e6d384c59c37d5e5ddc6220
Filipe Regadas <filiperegadas@gmail.com>
2021-05-01 07:38:47 +0100

    Use tree-sitter

--- config.el
+++ config.el
@@ -128,3 +128,9 @@
   (lsp-register-custom-settings
    '(("gopls.completeUnimported" t t)
      ("gopls.staticcheck" t t))))
+
+(use-package! tree-sitter
+  :config
+  (require 'tree-sitter-langs)
+  (global-tree-sitter-mode)
+  (add-hook 'tree-sitter-after-on-hook #'tree-sitter-hl-mode))
--- packages.el
+++ packages.el
@@ -14,6 +14,8 @@
 (package! kubernetes)
 (package! ob-mermaid)
 (package! ob-http)
+(package! tree-sitter)
+(package! tree-sitter-langs)
 
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
