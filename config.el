93ffa058efdc2ccf655d2a8eeed8d57c737f30da
Filipe Regadas <filiperegadas@gmail.com>
2021-05-22 13:19:29 +0100

    Fix org-mode

--- config.el
+++ config.el
@@ -52,6 +52,7 @@
 ;; company-minimum-prefix-length 0)
 
 (auto-save-visited-mode +1)
+(add-hook 'org-mode-hook (lambda () (electric-indent-local-mode -1)))
 
 ;; Here are some additional functions/macros that could help you configure Doom:
 ;;
