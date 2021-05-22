b0f6a524ebba9b3843255c69521a61dd5c095b47
Filipe Regadas <filiperegadas@gmail.com>
2021-05-22 13:21:25 +0100

    Update theme

--- config.el
+++ config.el
@@ -25,7 +25,7 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-homage-white
+(setq doom-theme 'doom-gruvbox
       doom-font (font-spec :family "Iosevka Term" :size 16)
       doom-themes-enable-bold t
       doom-themes-enable-italic t)
