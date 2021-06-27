ee5fe20e85ee9ea8dc08410a238bffc9faea6739
Filipe Regadas <filiperegadas@gmail.com>
2021-06-27 07:49:26 +0100

    Add modus-themes

--- packages.el
+++ packages.el
@@ -16,6 +16,7 @@
 (package! ob-http)
 (package! tree-sitter)
 (package! tree-sitter-langs)
+(package! modus-themes)
 
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
