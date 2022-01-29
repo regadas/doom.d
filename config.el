838e010b777bd94d64f7d12ecfb127bbe434b9d5
Filipe Regadas <filiperegadas@gmail.com>
2022-01-29 07:03:10 +0000

    Update theme

--- config.el
+++ config.el
@@ -27,8 +27,8 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'modus-operandi
-      doom-font (font-spec :family "Iosevka Term SS07" :size 15 :weight 'Medium))
+(setq doom-theme 'doom-tokyo-night
+      doom-font (font-spec :family "Iosevka Term SS07" :size 16 :weight 'Medium))
 
 (setq-default line-spacing 3)
 
