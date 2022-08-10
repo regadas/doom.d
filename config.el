0b86da8c9c4439ef200e46a6686fa073047fccea
Filipe Regadas <filiperegadas@gmail.com>
2022-08-10 14:57:36 +0100

    Update theme

--- config.el
+++ config.el
@@ -27,7 +27,7 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'catppuccin
+(setq doom-theme 'doom-solarized-light
       doom-font (font-spec :family "Iosevka" :size 16 :weight 'Medium))
 
 (setq doom-fallback-buffer-name "► Doom"
