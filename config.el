5f9756ffd51e398cae7150f9f36f7a30cfed8e87
Filipe Regadas <filiperegadas@gmail.com>
2022-08-23 10:35:23 +0100

    Update theme and font

--- config.el
+++ config.el
@@ -29,8 +29,8 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-tokyo-night
-      doom-font (font-spec :family "Iosevka" :size 16 :weight 'Medium))
+(setq doom-theme 'catppuccin
+      doom-font (font-spec :family "Iosevka" :size 16))
 
 (setq doom-fallback-buffer-name "► Doom"
       +doom-dashboard-name "► Doom")
