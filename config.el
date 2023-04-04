02704f4d5b688aa89be0da79e3cdcbf9651fc6fd
regadas <oss@regadas.email>
2023-04-04 12:46:25 +0100

    Update copilot config

--- config.el
+++ config.el
@@ -150,12 +150,12 @@
 
 ;; copilot.el
 (use-package! copilot
-  :hook ((prog-mode yaml-mode) . copilot-mode)
-  :bind (("C-TAB" . 'copilot-accept-completion-by-word)
-         ("C-<tab>" . 'copilot-accept-completion-by-word)
-         :map copilot-completion-map
-         ("<tab>" . 'copilot-accept-completion)
-         ("TAB" . 'copilot-accept-completion)))
+  :hook ((prog-mode yaml-mode text-mode) . copilot-mode)
+  :bind (:map copilot-completion-map
+              ("<tab>" . 'copilot-accept-completion)
+              ("TAB" . 'copilot-accept-completion)
+              ("C-TAB" . 'copilot-accept-completion-by-word)
+              ("C-<tab>" . 'copilot-accept-completion-by-word)))
 
 (use-package! ob-sql-mode
   :after org)
