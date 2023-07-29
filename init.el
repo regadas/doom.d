1c0bea666198f76c74700dab2dcb283103d59af9
regadas <oss@regadas.email>
2023-07-29 10:05:20 +0100

    Disable vi-tilde-fringe

--- init.el
+++ init.el
@@ -48,6 +48,7 @@
        ;;unicode           ; extended unicode support for various languages
        (vc-gutter +pretty +diff-hl)         ; vcs diff in the fringe
        vi-tilde-fringe   ; fringe tildes to mark beyond EOB
+       ;; vi-tilde-fringe   ; fringe tildes to mark beyond EOB
        (window-select +numbers)     ; visually switch windows
        workspaces        ; tab emulation, persistence & separate workspaces
        ;; zen               ; distraction-free coding or writing
