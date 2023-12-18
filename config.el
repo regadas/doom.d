c20072fb56eb7941c639a6b871cf872d3f9568cd
regadas <oss@regadas.email>
2023-12-18 18:49:50 +0000

    fix emacs-mac big title bar

--- config.el
+++ config.el
@@ -3,6 +3,7 @@
 ;; Place your private configuration here! Remember, you do not need to run 'doom
 ;; sync' after modifying this file!
 
+(add-hook 'doom-after-init-hook (lambda () (tool-bar-mode 1) (tool-bar-mode 0)))
 
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets.
