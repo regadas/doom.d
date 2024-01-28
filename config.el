59c63aa4d9ca13489ce8664736b5ab60f4307aba
regadas <oss@regadas.email>
2024-01-28 09:20:51 +0000

    Remove gc properties and stick with default values

--- config.el
+++ config.el
@@ -31,18 +31,13 @@
 (setq doom-theme 'modus-operandi
       doom-font (font-spec :family "Iosevka SS09" :size 16))
 
-(setq gcmh-high-cons-threshold (* 1024 1024 1024)
-      undo-limit 80000000                         ; Raise undo-limit to 80Mb
+(setq undo-limit 80000000                         ; Raise undo-limit to 80Mb
       evil-want-fine-undo t                       ; By default while in insert all changes are one big blob. Be more granular
       auto-save-default t                         ; Nobody likes to loose work, I certainly don't
       truncate-string-ellipsis "…"               ; Unicode ellispis are nicer than "...", and also save /precious/ space
       password-cache-expiry nil                   ; I can trust my computers ... can't I?
       ;; scroll-preserve-screen-position 'always  ; Don't have `point' jump around
-      scroll-margin 2                            ; It's nice to maintain a little margin
-      read-process-output-max (* 128 1024 1024)
-      process-adaptive-read-buffering nil)
-;; Prevents some cases of Emacs flickering
-;; (add-to-list 'default-frame-alist '(inhibit-double-buffering . t))
+      scroll-margin 2)                            ; It's nice to maintain a little margin
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
