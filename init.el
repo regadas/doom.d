c1c4c97ee1afcefd9384046e5951b91ec8d7b711
regadas <oss@regadas.email>
2024-02-06 12:51:36 +0000

    Disable gist

--- init.el
+++ init.el
@@ -93,7 +93,7 @@
        editorconfig      ; let someone else argue about tabs vs spaces
        ;; ein               ; tame Jupyter notebooks with emacs
        (eval +overlay)     ; run code, run (also, repls)
-       gist              ; interacting with github gists
+       ;; gist              ; interacting with github gists
        lookup              ; navigate your code and its documentation
        lsp
        (magit +forge)            ; a git porcelain for Emacs
