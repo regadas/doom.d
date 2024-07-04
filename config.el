259efcb3fa68a4210a2b0f96012d18ec1079faa7
regadas <oss@regadas.email>
2024-07-04 01:01:28 +0100

    Back to the goat JetBrains Mono font

--- config.el
+++ config.el
@@ -31,7 +31,7 @@
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
 (setq doom-theme 'modus-operandi
-      doom-font (font-spec :family "Comic Code Ligatures" :size 14))
+      doom-font (font-spec :family "JetBrains Mono" :size 15))
 
 (after! modus-themes
   (setq modus-themes-italic-constructs t
