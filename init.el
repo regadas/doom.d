1f3e6e5472384a1dcada203fe4b1401089cb8417
regadas <oss@regadas.email>
2024-08-31 04:45:45 -0400

    Disable corfu orderless

--- init.el
+++ init.el
@@ -21,7 +21,7 @@
 
        :completion
        ;; (company +childframe)           ; the ultimate code completion backend
-       (corfu +icons +orderless)
+       (corfu +icons)
        ;;helm              ; the *other* search engine for love and life
        ;;ido               ; the other *other* search engine...
        ;;(ivy +fuzzy +icons)               ; a search engine for love and life
